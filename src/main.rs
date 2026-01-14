9f1d1f4a3d86fcd300f634e94aada9cee1216298
Zane Gant <zane.gant@gmail.com>
2026-01-13 22:22:53 -0800

    made a simple stopwatch program

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,22 @@ mod util;
 // each view only needs to interpolate against itself
 // list, and grid views are some examples
 
+enum StartStop {
+    Start(std::time::Instant),
+    Stop(std::time::Instant),
+}
+
+impl std::fmt::Debug for StartStop {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            // Self::Start(arg0) => f.debug_tuple("Start").field(arg0).finish(),
+            // Self::Stop(arg0) => f.debug_tuple("Stop").field(arg0).finish(),
+            Self::Start(_) => f.debug_tuple("Start").finish(),
+            Self::Stop(_) => f.debug_tuple("Stop").finish(),
+        }
+    }
+}
+
 struct DevWindow<'a> {
     window_id: winit::window::WindowId,
     surface: wgpu::Surface<'a>,
@@ -24,6 +40,9 @@ struct DevWindow<'a> {
 
     viewport: glyphon::Viewport,
     text_buffer: glyphon::Buffer,
+
+    starts_and_stops: Vec<StartStop>,
+    _fps_thread: std::thread::JoinHandle<()>,
 }
 
 impl DevWindow<'_> {
@@ -59,6 +78,8 @@ impl DevWindow<'_> {
             glyphon::Metrics::new(30.0, 42.0),
         );
 
+        let weak_window = std::sync::Arc::downgrade(window);
+
         DevWindow {
             window_id: window.id(),
             surface,
@@ -67,6 +88,14 @@ impl DevWindow<'_> {
             texture_view_descriptor,
             viewport,
             text_buffer,
+            starts_and_stops: Vec::new(),
+            _fps_thread: std::thread::spawn(move || {
+                while let Some(window) = weak_window.upgrade() {
+                    window.request_redraw();
+                    drop(window);
+                    std::thread::sleep(std::time::Duration::from_secs(1));
+                }
+            }),
         }
     }
 }
@@ -85,6 +114,30 @@ impl app::WindowHandler<AppState> for DevWindow<'_> {
             } => {
                 _ = window.drag_window();
             }
+            winit::event::WindowEvent::KeyboardInput {
+                event:
+                    winit::event::KeyEvent {
+                        text: Some(text),
+                        state: winit::event::ElementState::Pressed,
+                        ..
+                    },
+                ..
+            } => match text.as_str() {
+                " " => {
+                    window.request_redraw();
+                    self.starts_and_stops
+                        .push(match self.starts_and_stops.last() {
+                            Some(StartStop::Start(_)) => StartStop::Stop(std::time::Instant::now()),
+                            Some(StartStop::Stop(_)) => StartStop::Start(std::time::Instant::now()),
+                            None => StartStop::Start(std::time::Instant::now()),
+                        })
+                }
+                "r" => {
+                    window.request_redraw();
+                    self.starts_and_stops.clear();
+                }
+                s => println!("{s}"),
+            },
             winit::event::WindowEvent::Resized(size) => {
                 self.surface_config.width = size.width;
                 self.surface_config.height = size.height;
@@ -100,17 +153,28 @@ impl app::WindowHandler<AppState> for DevWindow<'_> {
                 );
             }
             winit::event::WindowEvent::RedrawRequested => {
+                let total: std::time::Duration = self
+                    .starts_and_stops
+                    .chunks(2)
+                    .map(|s| match s {
+                        [StartStop::Start(a), StartStop::Stop(b)] => b.duration_since(*a),
+                        [StartStop::Start(a)] => a.elapsed(),
+                        a => {
+                            todo!("was not expecting {a:?}")
+                        }
+                    })
+                    .sum();
+
+                let seconds = total.as_secs() % 60;
+                let minutes = (total.as_secs() / 60) % 60;
+                let hours = (total.as_secs() / 60) / 60;
+
                 self.text_buffer.set_text(
                     &mut app_state.font_system,
-                    format!(
-                        "inner_size = {:?}\nouter_size = {:?}",
-                        window.inner_size(),
-                        window.outer_size()
-                    )
-                    .as_str(),
+                    format!("{hours}:{minutes}:{seconds}\n{:?}", self.starts_and_stops).as_str(),
                     &glyphon::Attrs::new(),
                     glyphon::Shaping::Advanced,
-                    None,
+                    Some(glyphon::cosmic_text::Align::Center),
                 );
 
                 let size = window.inner_size().cast::<f32>();
@@ -134,7 +198,7 @@ impl app::WindowHandler<AppState> for DevWindow<'_> {
                             buffer: &self.text_buffer,
                             left: 0.0,
                             top: 0.0,
-                            scale: 1.0,
+                            scale: scale_factor,
                             bounds: glyphon::TextBounds {
                                 left: 0,
                                 top: 0,
@@ -243,9 +307,7 @@ impl AppState {
 
 fn main() {
     let event_loop = winit::event_loop::EventLoop::builder().build().unwrap();
-    event_loop.set_control_flow(winit::event_loop::ControlFlow::wait_duration(
-        std::time::Duration::from_secs_f64(1.0 / 30.0),
-    ));
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
 
     let mut app = app::App::new(
         smol::block_on(AppState::new()),
