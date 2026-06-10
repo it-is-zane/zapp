ebcfe2e182b57383388406793319b33ccbefe27a
Zane Gant <zane.gant@gmail.com>
2026-06-10 14:20:14 -0700

    cleaning up project
    i want to rethink how i am doing this. i don't know if i even like the idea i have going here. in particular the origional idea was to make it so windows handlers were only exposed to their part of the app, but i also wanted to keep zapp thin. this lead to a lot of weird stuff like window handlers basically just had to promis not to mess with each other with nothing really stopping them from doing so. also it is weird how wgpu should be managed. should this crate package wgpu or should creats using it package it themselves. these are all things i need to think through a lot more. so this is going into testing mode i guess.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,68 @@
-pub mod app;
-pub mod render;
-pub mod util;
+struct GpuContext {
+    instance: wgpu::Instance,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+}
 
-pub use smol;
-pub use wgpu;
-pub use winit;
+impl GpuContext {
+    async fn new() -> Self {
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptionsBase::default())
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor::default())
+            .await
+            .unwrap();
+
+        Self {
+            instance,
+            adapter,
+            device,
+            queue,
+        }
+    }
+}
+
+trait WindowHandler {
+    fn redraw(&mut self, pass: &mut wgpu::RenderPass) {}
+}
+
+struct WindowContext {
+    window_handler: Box<dyn WindowHandler>,
+}
+
+pub struct App {
+    gpu_context: Option<GpuContext>,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self { gpu_context: None }
+    }
+
+    pub fn run() -> Result<(), winit::error::EventLoopError> {
+        let event_loop = winit::event_loop::EventLoop::builder().build().unwrap();
+        event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
+        event_loop.run_app(&mut Self::new())
+    }
+}
+
+impl winit::application::ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        self.gpu_context = Some(smol::block_on(GpuContext::new()));
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        todo!()
+    }
+}
