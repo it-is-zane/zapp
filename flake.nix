a6530e44bbe9f46985420bc5a8202aaa45264643
Zane Gant <zane.gant@gmail.com>
2026-06-10 14:05:14 -0700

    updated flake

--- flake.nix
+++ flake.nix
@@ -44,6 +44,7 @@
           shellHook = ''
             export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
             export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+            exec fish
           '';
 
           # Add precompiled library to rustc search path
